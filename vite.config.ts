7a46a54b9218579e15fd2ed1d153692d5f6cd56b
zyy0098 <770823669@qq.com>
2024-10-25 17:04:55 -0400

    docker test

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 	server: { 
-    host: '0.0.0.0',  // This binds the dev server to all available network interfaces
+    host: '::',  // This binds the dev server to all available network interfaces
     port: 5173 ,
     proxy: { 
       '/api': { 
