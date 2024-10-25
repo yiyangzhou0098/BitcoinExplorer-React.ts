d052b61945bd933a43f73009924cc5b97a076a76
zyy0098 <770823669@qq.com>
2024-10-25 16:42:43 -0400

    bind dev server to all network interfaces

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
 	server: { 
+    host: '0.0.0.0',  // This binds the dev server to all available network interfaces
+    port: 5173 ,
     proxy: { 
       '/api': { 
         target: 'http://localhost:3030', }, }, }, });
